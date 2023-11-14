9ca3684c954605e2ddde1e22039ddce66f48bad9
Wenxi Zhang <zhang.wenxi1@northeastern.edu>
2023-11-14 10:30:51 -0500

    api error

--- src/Kanbas/Courses/Assignments/client.js
+++ src/Kanbas/Courses/Assignments/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const COURSES_URL = "http://localhost:4000/api/courses";
-const ASSIGNMENT_URL = "http://localhost:4000/api/assignments";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const ASSIGNMENT_URL = `${API_BASE}/assignments`;
+const COURSES_URL = `${API_BASE}/courses`;
 
 export const findAssignmentsForCourse = async (courseId) => {
     const response = await axios
